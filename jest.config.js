59cff85bc506c6c8b4a0c134d182c4e297f97921
juanan0309 <juan.quiroz@wizeline.com>
2022-04-04 09:56:22 -0500

    First try doing tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/styles.ts",
+    "\\.(css|less)$": "identity-obj-proxy",
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  },
+  testEnvironment: 'jsdom',
+  '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
 }
